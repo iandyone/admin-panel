f6f349f71f4a56c42664dcc60a00eb8f32fb2c3d
Andrei Bernatski <iandyone.dev@gmail.com>
2025-07-23 13:30:08 +0300

    init(api): app module & prisma

--- services/admin-panel-api/src/main.ts
+++ services/admin-panel-api/src/main.ts
@@ -1,10 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app.module';
+import { AppModule } from 'src/modules/app/app.module';
+import { AppConfig } from 'src/types';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  const configService: ConfigService<AppConfig> = app.get(ConfigService);
+
+  await app.listen(configService.get('PORT_API'));
 }
 
 bootstrap();
--- services/admin-panel-api/src/modules/app/app.module.ts
+++ services/admin-panel-api/src/modules/app/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      // compose will load a .env file from root dir
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
