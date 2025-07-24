2dd12538807cd917f85031f095cf2ff093f110fd
Andrei Bernatski <iandyone.dev@gmail.com>
2025-07-24 17:43:29 +0300

    fix(api): relation imports

--- services/admin-panel-api/src/main.ts
+++ services/admin-panel-api/src/main.ts
@@ -1,13 +1,14 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from 'src/modules/app/app.module';
-import { AppConfig } from 'src/types';
+
+import { AppModule } from './modules/app/app.module';
+import { AppConfig } from './types';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService<AppConfig> = app.get(ConfigService);
 
-  await app.listen(configService.get('PORT_API'));
+  await app.listen(process.env.PORT || configService.get('PORT_API'));
 }
 
 bootstrap();
