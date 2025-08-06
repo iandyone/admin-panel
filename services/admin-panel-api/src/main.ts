00900b6bd8a1a11bf06eed9b5eebe60c1783eccf
Andrei Bernatski <iandyone.dev@gmail.com>
2025-08-06 12:24:16 +0300

    chore(api): add cors origins

--- services/admin-panel-api/src/main.ts
+++ services/admin-panel-api/src/main.ts
@@ -8,6 +8,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService<AppConfig> = app.get(ConfigService);
 
+  app.enableCors({
+    origin: process.env.ORIGINS.split(',') ?? [],
+    methods: 'GET,HEAD,PATCH,PUT,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
   await app.listen(configService.get('PORT_API') || process.env.PORT);
 }
 
